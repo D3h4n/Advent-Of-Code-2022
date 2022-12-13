bd20b8f530cac56980502488f26734ee0a4d3254
D3h4n <shawn2012tt@live.com>
2022-12-12 21:02:13 -0500

    Day 6

---
+++ Day 6/Main.java
@@ -0,0 +1,56 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import java.util.HashSet;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String input;
+        String fileName = args[1];
+        int length = Integer.parseInt(args[2]);
+
+        try {
+            input = readFile(fileName);
+        } catch (FileNotFoundException e) {
+            System.err.println(e);
+            return;
+        }
+        
+        System.out.println("Result: " + findMarkerPosition(input, length));
+    }
+
+    static String readFile(String fileName) throws FileNotFoundException {
+        StringBuilder input = new StringBuilder();
+        File f = new File(fileName);
+        Scanner scanner = new Scanner(f);
+        
+        while(scanner.hasNextLine()) {
+            input.append(scanner.nextLine());
+        }
+
+        scanner.close();
+
+        return input.toString();
+    }
+
+    static int findMarkerPosition(String input, int length) {
+        HashSet<String> set = new HashSet<String>();
+
+        if (input.length() < length) 
+            return -1;
+
+        for (int i = 0; i < input.length() - length; i++) {
+            String[] chars = input.substring(i, i + length).split("");
+            set.addAll(Arrays.asList(chars));
+            
+            if (set.size() == length)
+                return i + length;
+
+            set.clear();
+        }
+
+        return -1;
+    }
+}
