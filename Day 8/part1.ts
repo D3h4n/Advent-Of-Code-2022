cf95e13966df76611486cbd8cb81aa49956722df
D3h4n <shawn2012tt@live.com>
2022-12-15 20:06:52 -0500

    Day 8

---
+++ Day 8/part1.ts
@@ -0,0 +1,111 @@
+async function main() {
+  const text = await Deno.readTextFile("puzzle_input.txt");
+  const trees = text
+    .split("\n")
+    .map((line) => line.split(""))
+    .map((row) => row.map(Number))
+    .slice(0, -1);
+  const count = countVisibleTreesInGrid(trees);
+  console.log(count);
+}
+
+export function countVisibleTreesInGrid(trees: number[][]): number {
+  return numPerimTrees(trees) + numVisibleInnerTrees(trees);
+}
+
+function numPerimTrees(trees: number[][]): number {
+  return 2 * (trees.length + trees[0].length - 2);
+}
+
+function numVisibleInnerTrees(trees: number[][]): number {
+  let count = 0;
+
+  for (let y = 1; y < trees.length - 1; y++) {
+    for (let x = 1; x < trees[y].length - 1; x++) {
+      if (treeIsVisible(x, y, trees)) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
+
+function treeIsVisible(x: number, y: number, trees: number[][]): boolean {
+  return (
+    treeIsVisibleFromWest(x, y, trees) ||
+    treeIsVisibleFromEast(x, y, trees) ||
+    treeIsVisibleFromNorth(x, y, trees) ||
+    treeIsVisibleFromSouth(x, y, trees)
+  );
+}
+
+function treeIsVisibleFromWest(
+  x: number,
+  y: number,
+  trees: number[][],
+): boolean {
+  const row = trees[y];
+  const tree = row[x];
+
+  for (let i = x - 1; i >= 0; i--) {
+    if (row[i] >= tree) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function treeIsVisibleFromEast(
+  x: number,
+  y: number,
+  trees: number[][],
+): boolean {
+  const row = trees[y];
+  const tree = row[x];
+
+  for (let i = x + 1; i < row.length; i++) {
+    if (row[i] >= tree) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function treeIsVisibleFromNorth(
+  x: number,
+  y: number,
+  trees: number[][],
+): boolean {
+  const tree = trees[y][x];
+
+  for (let i = y - 1; i >= 0; i--) {
+    if (trees[i][x] >= tree) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function treeIsVisibleFromSouth(
+  x: number,
+  y: number,
+  trees: number[][],
+): boolean {
+  const tree = trees[y][x];
+
+  for (let i = y + 1; i < trees.length; i++) {
+    if (trees[i][x] >= tree) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+if (import.meta.main) {
+  main();
+}
---
+++ Day 8/part2.ts
@@ -0,0 +1,111 @@
+async function main() {
+  const text = await Deno.readTextFile("puzzle_input.txt");
+  const trees = text
+    .split("\n")
+    .map((line) => line.split(""))
+    .map((row) => row.map(Number))
+    .slice(0, -1);
+  const max = findMaxScenicScore(trees);
+  console.log(max);
+}
+
+function findMaxScenicScore(trees: number[][]): number {
+  let max = -1;
+
+  for (let i = 1; i < trees.length - 1; i++) {
+    for (let j = 1; j < trees[i].length - 1; j++) {
+      const scenicScore = calculateScenicScore(i, j, trees);
+
+      if (scenicScore > max) {
+        max = scenicScore;
+      }
+    }
+  }
+
+  return max;
+}
+
+function calculateScenicScore(i: number, j: number, trees: number[][]): number {
+  return calculateScenicScoreNorth(i, j, trees) *
+    calculateScenicScoreSouth(i, j, trees) *
+    calculateScenicScoreEast(i, j, trees) *
+    calculateScenicScoreWest(i, j, trees);
+}
+
+function calculateScenicScoreNorth(
+  i: number,
+  j: number,
+  trees: number[][],
+): number {
+  let count = 0;
+  const tree = trees[i][j];
+
+  for (let y = i - 1; y >= 0; y--) {
+    count++;
+    if (trees[y][j] >= tree) {
+      break;
+    }
+  }
+
+  return count;
+}
+
+function calculateScenicScoreSouth(
+  i: number,
+  j: number,
+  trees: number[][],
+): number {
+  let count = 0;
+  const tree = trees[i][j];
+
+  for (let y = i + 1; y < trees.length; y++) {
+    count++;
+    if (trees[y][j] >= tree) {
+      break;
+    }
+  }
+
+  return count;
+}
+
+function calculateScenicScoreEast(
+  i: number,
+  j: number,
+  trees: number[][],
+): number {
+  let count = 0;
+  const row = trees[i];
+  const tree = row[j];
+
+  for (let x = j + 1; x < row.length; x++) {
+    count++;
+    if (row[x] >= tree) {
+      break;
+    }
+  }
+
+  return count;
+}
+
+function calculateScenicScoreWest(
+  i: number,
+  j: number,
+  trees: number[][],
+): number {
+  let count = 0;
+  const row = trees[i];
+  const tree = row[j];
+
+  for (let x = j - 1; x >= 0; x--) {
+    count++;
+    if (row[x] >= tree) {
+      break;
+    }
+  }
+
+  return count;
+}
+
+if (import.meta.main) {
+  main();
+}
